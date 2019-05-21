f57cd4ee6814a5f470d5750dab1228e5a75f2201
HowardDennis <h2dennis@gmail.com>
2019-05-21 16:11:35 -0400

    Updated User Controller to have getting and deleting

--- src/main/java/com/revature/controllers/UserController.java
+++ src/main/java/com/revature/controllers/UserController.java
@@ -1,10 +1,18 @@
 package com.revature.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 
 import com.revature.models.User;
 import com.revature.services.UserServices;
@@ -24,4 +32,45 @@ public class UserController {
 	public void createUser(@RequestBody User user) {
 		userServices.createUser(user);
 	}
+	
+	@GetMapping("")
+	public List<User> getAllUsers() {
+		return userServices.getAllUsers();
+	}
+	
+	@GetMapping("/{id}")
+	public User getUserById(@PathVariable int id) {
+		return Optional.ofNullable(this.userServices.getUserById(id))
+				.orElseThrow(() -> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deleteUserById(@PathVariable int id) {
+		System.out.println(userServices.deleteUserById(id));
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
