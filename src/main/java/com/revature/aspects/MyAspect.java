b23032949af4d6a0814e97442907248e0f681c44
amnaazmi <amnaazmi95@gmail.com>
2019-05-20 21:33:31 -0400

    added the log 4 j and log4j dependencies

---
+++ src/main/java/com/revature/aspects/MyAspect.java
@@ -0,0 +1,17 @@
+package com.revature.aspects;
+
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyAspect {
+
+
+	
+	Logger log = Logger.getRootLogger();
+	
+
+}
